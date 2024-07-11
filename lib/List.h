1d852642df1217b1b3a0e2317de02a28ddd84b09
fcharles <fcharles@ucsc.edu>
2024-07-11 00:11:09 -0700

    Added the definition of template list to header file instead of .cpp file. Finished up to insert_right method.

--- lib/List.h
+++ lib/List.h
@@ -4,12 +4,11 @@
 ///////////////////////////////////////////////
 #ifndef LIST_H
 #define LIST_H
+#include <optional>
+#include <cstddef>
 
-
-
-
-
-template <typename T>
+/** Class definition **/
+template <class T>
 class List {
 private:
 	struct Node {
@@ -27,7 +26,7 @@ private:
 	Node *after_cursor;
 	Node *before_cursor;
 	size_t cursor_pos;
-	size_t len; // length of list
+	size_t length; // length of list
 
 public:
 	/*Constructors*/
@@ -51,14 +50,14 @@ public:
 	* @preq: List must not be empty
 	* @return type T
 	*/
-	T front() const;
+	std::optional<T> front() const;
 
 	/**
 	* @brief returns back element if present
 	* @preq: List must not be empty
 	* @return type T
 	*/
-	T back() const;
+	std::optional<T> back() const;
 
 	/**
 	* @brief returns position of cursor 0<=cursor_position<=list.len()
@@ -71,14 +70,14 @@ public:
 	* @preq: cursor_position() < len()
 	* @return type T
 	*/
-	T peek_right() const;
+	std::optional<T> peek_right() const;
 
 	/**
 	* @brief check left element of cursor
 	* @preq: cursor_position() > 0
 	* @return type T
 	*/
-	T peek_left() const;
+	std::optional<T> peek_left() const;
 
 	/*Manipulation Functions*/
 
@@ -90,12 +89,12 @@ public:
 	/**
 	* @brief sets cursor_pos = 0
 	*/
-	void cursor_move_head();
+	void cursor_move_to_head();
 
 	/**
 	* @brief sets cursor_pos = len()
 	*/
-	void cursor_move_tail();
+	void cursor_move_to_tail();
 
 	/**
 	* @brief move the cursor 1 position right
@@ -113,13 +112,13 @@ public:
 	* @brief Insert new element to right of cursor
 	*
 	*/
-	void insert_right();
+	void insert_right(T val);
 
 	/**
 	* @brief Insert new element to left of cursor
 	*
 	*/
-	void insert_left();
+	void insert_left(T val);
 
 	/**
 	* @brief Overwrite right element with value
@@ -144,9 +143,190 @@ public:
 	* @preq len() != 0 && cursor_pos > 0
 	*/
 	void erase_left();
-
 }
 
+/** Template Node Implementation **/
+template<class T>
+List<T>::Node::Node(T val): val(val), left_node(nullptr), right_node(nullptr) {};
+
+/** Template List Implementation **/
+
+/*Constructor*/
+template<class T>
+List<T>::List() {
+	head = nullptr;
+	tail = nullptr;
+	after_cursor = nullptr;
+	before_cursor = nullptr;
+	cursor_pos = 0;
+	len = 0;
+}
+
+/*length*/
+template<class T>
+size_t List<t>::len() const {
+	return this->length;
+}
+
+/*head value*/
+template<class T>
+std::optional<T> List<T>::front() const {
+	if (this->head == nullptr)
+	{
+		return std::nullopt;
+	}
+
+	return this->head->val;
+}
+
+/*tail value*/
+template<class T>
+std::optional<T> List<T>::back() const {
+	if (this->tail == nullptr)
+	{
+		return std::nullopt;
+	}
+
+	return this->tail->val;
+}
+
+/*cursor position*/
+template<class T>
+size_t List<T>::cursor_position() const {
+	return this->cursor_pos;
+}
+
+/*peek_right*/
+template<class T>
+std::optional<T> List<T>::peek_right() const {
+	if (this->cursor_pos == this->length)
+	{
+		return std::nullopt;
+	}
+	return this->after_cursor->val;
+}
+
+/*peek left*/
+template<class T>
+std::optional<T> List<T>::peek_left() const {
+	if (this->cursor_pos == 0)
+	{
+		return std::nullopt;
+	}
+	return this->before_cursor->val;
+}
+
+/** Manipulation **/
+
+/*Clear list*/
+template<class T>
+void List<T>::clear() {
+	this->length = 0;
+	this->cursor_pos = 0;
+	this->after_cursor = nullptr;
+	this->before_cursor = nullptr;
+
+	// Deletes all elements 1 by 1
+	while (this->head != nullptr)
+	{
+		Node *holder = this->head->left_node;
+
+		this->head->left_node = nullptr;
+
+		delete this->head;
+
+		if (holder != nullptr)
+		{
+			holder->right_node = nullptr;
+		}
+		this->head = holder;
+	}
+
+	this->tail = nullptr;
+}
+
+/*cursor move to head*/
+template<class T>
+void List<T>::cursor_move_to_head() {
+	this->before_cursor = nullptr;
+	this->after_cursor = this->head;
+	this->cursor_pos = 0;
+}
+
+/*cursor move to tail*/
+template<class T>
+void List<T>::cursor_move_to_tail() {
+	this->after_cursor = nullptr;
+	this->before_cursor = this->tail;
+	this->cursor_pos = this->length;
+}
+
+/*move cursor right*/
+template<class T>
+void List<T>::move_right() {
+	if (this->cursor_pos == this->length)
+	{
+		return;
+	}
+
+	this->before_cursor = this->after_cursor;
+	this->after_cursor = this->after_cursor->right_node;
+	this->cursor_pos += 1;
+}
+
+/*move cursor left*/
+template<class T>
+void List<T>::move_left() {
+	if (this->cursor_pos == 0)
+	{
+		return;
+	}
+
+	this->after_cursor = this->before_cursor;
+	this->before_cursor = this->before_cursor->left_node;
+	this->cursor_pos -= 1;
+}
+
+/*insert to right of cursor*/
+template<class T>
+void List<T>::insert_right(T val) {
+	// Creating the node
+	Node *new_node = new Node(val);
+
+	if (this->cursor_pos == 0)
+	{
+		if (this->head != nullptr)
+		{
+			this->head->left_node = new_node;
+			new_node->right_node = this->head;
+		}
+		else
+		{
+			this->tail = new_node;
+		}
+		this->head = new_node;
+	}
+	else if (this->cursor_pos == this->length)
+	{
+		this->tail->right_node = new_node;
+		new_node->left_node = this->tail;
+		this->tail = new_node;
+	}
+	else
+	{
+		// Fix left node
+		this->before_cursor->right_node = new_node;
+		new_node->left_node = this->before_cursor;
+
+		// Fix right node
+		this->after_cursor->left_node = new_node;
+		new_node->right_node = this->after_cursor;
+	}
+
+	this->after_cursor = new_node;
+
+	this->length += 1;
+}
 
 
 
